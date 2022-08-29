8582962f3dc0c1839e9e807ef5c2973d7f23dc23
HimanshuPantwal <100841797+HimanshuPantwal@users.noreply.github.com>
2022-08-29 15:13:40 +0530

    Time: 40 ms (35.58%), Space: 27.7 MB (44.59%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+        unordered_map< int,int > mpp1,mpp2,mpp3;
+        unordered_set< int > st;
+        for(auto n:nums1)mpp1[n]+=1;
+        for(auto n:nums2)mpp2[n]+=1;
+        for(auto n:nums3)mpp3[n]+=1;
+        for(auto m:mpp1){
+            if(mpp2.find(m.first)!=mpp2.end()) st.emplace(m.first);
+            if(mpp3.find(m.first)!=mpp3.end()) st.emplace(m.first);
+        }
+        for(auto m:mpp2){
+            if(mpp3.find(m.first)!=mpp3.end()) st.emplace(m.first);
+        }
+        vector< int > res;
+        for(auto s:st)res.push_back(s);
+        return res;
+    }
+};
