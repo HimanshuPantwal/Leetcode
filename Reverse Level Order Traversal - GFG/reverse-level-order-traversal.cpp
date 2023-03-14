478eb55c8e4ddece73383cc0670bc9584e66bcd0
HimanshuPantwal <100841797+HimanshuPantwal@users.noreply.github.com>
2023-03-14 21:51:25 +0530

    Added solution - LeetHub

---
+++ Reverse Level Order Traversal - GFG/reverse-level-order-traversal.cpp
@@ -0,0 +1,141 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+vector<int> reverseLevelOrder(Node* root);
+
+int main()
+{
+
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        vector<int> result = reverseLevelOrder(root);
+        for (int i = 0; i < result.size(); ++i)
+        {
+            cout<<result[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 1;
+}
+// } Driver Code Ends
+
+
+/*   
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+int height(Node* root)
+{
+    if(root==NULL) return 0;
+    int left=height(root->left);
+    int right=height(root->right);
+    return max(left,right)+1;
+}
+void find(Node* root,int x,int l,vector<int> &ans)
+{
+    if(root==NULL) return;
+    if(x==l) {ans.push_back(root->data); return;}
+    if(x<l){
+        find(root->left,x+1,l,ans);
+        find(root->right,x+1,l,ans);
+    }
+}
+vector<int> reverseLevelOrder(Node *root)
+{
+    int h=height(root);
+    vector<int> ans;
+    for(int i=h;i>=1;i--)
+    {
+        find(root,1,i,ans);
+    }
+    return ans;
+}
